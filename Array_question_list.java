ef015535df7a46d573ed8c176e6fb079f1b4b44e
AkshayEnginner <132028709+AkshayEnginner@users.noreply.github.com>
2023-04-30 17:12:26 +0530

    Add files via upload
    Write a program in Java for, In array 1-100 numbers are stored, one number is missing how do you find it.

---
+++ Array_question_list.java
@@ -0,0 +1,24 @@
+1.] Write a program in Java for, In array 1-100 numbers are stored, one number is missing how do you find it.
+2.]Write a program in Java for, In a array 1-100 multiple numbers are duplicates, how do you find it.
+3.]Write a program in Java for, How to find all pairs in array of integers whose sum is equal to given number.
+4.]Write a program in Java for, In a array 1-100 exactly one number is duplicate how do you find it.
+5.]Write a program in Java for, Given two arrays 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
+6.]Write a program in Java for, How to compare two array is equal in size or not.
+7.]Write a program in Java to find largest and smallest number in array.
+8.]Write a program in Java to find second highest number in an integer array.
+9.]Write a program in Java to find top two maximum number in array?
+10.]Java program to print array in reverse Order.
+11.]Java program to reverse array without using second array.
+12.]Java Program to calculate length of an array.
+13.]Java program to insert an element at end of an Array.
+14.]Java program to insert element at a given location in Array.
+15.]Java Program to delete element at end of Array.
+16.]Java Program to delete given element from Array.
+17.]Java Program to delete element from array at given index.
+18.]Java Program to find sum of array elements.
+19.]Java Program to print all even numbers in array.
+20.]Java Program to print all odd numbers in array.
+21.]Java program to perform left rotation of array elements by two positions.
+22.]Java program to perform right rotation in array by 2 positions.
+23.]Java program to find all pairs in array of integers whose sum is equal to given number.
+24.]Java Program to remove duplicate elements form array.
